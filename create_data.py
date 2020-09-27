84190c67e81610e1b97ed5bcc70c4b6a451ced3e
shulgao69 <shulgao69@mail.ru>
2020-09-27 16:56:08 +0300

    test2

--- create_data.py
+++ create_data.py
@@ -3,16 +3,16 @@ import json
 from app import Teacher, db, Goal, Day
 from data import teachers, days, goals
 
-#
-# for key, value in days.items():
-#     day = Day(day_key_en = key, day_value_ru = value)
-#     db.session.add(day)
-# db.session.commit()
-#
-# for key, value in goals.items():
-#     goal = Goal(goal_key_en = key, goal_value_ru = value)
-#     db.session.add(goal)
-# db.session.commit()
+
+for key, value in days.items():
+    day = Day(day_key_en = key, day_value_ru = value)
+    db.session.add(day)
+db.session.commit()
+
+for key, value in goals.items():
+    goal = Goal(goal_key_en = key, goal_value_ru = value)
+    db.session.add(goal)
+db.session.commit()
 
 for teacher in teachers:
     teacher_record = Teacher(id = teacher['id'], name = teacher['name'], about = teacher['about'], rating = teacher['rating'], picture = teacher['picture'], price = teacher['price'], free = json.dumps(teacher['free']))
